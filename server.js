a176a7d9e3154f9d392d542efa9e190512b0167e
novasm55 <novasmichael93@gmail.com>
2023-04-11 11:52:55 -0700

    starter code restart

--- server.js
+++ server.js
@@ -1,56 +1,70 @@
+// Import the required modules
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const { v4: uuidv4 } = require('uuid');
 
+// Initialize the Express app
 const app = express();
+
+// Set the port for the server to listen on, either from the environment variable or default to 3001
 const PORT = process.env.PORT || 3001;
 
-app.use(express.json());
+// Set up Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Serve static files (CSS, JavaScript, etc.) from the 'public' directory
 app.use(express.static('public'));
 
-app.get('/notes', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
-);
+// HTML routes
 
+// Route to serve the notes.html file
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, '/public/notes.html'));
+});
+
+
+// API routes
+
+// Read the db.json file and return all saved notes as JSON
 app.get('/api/notes', (req, res) => {
-  fs.readFile('./db/db.json', 'utf-8', (err, data) => {
-    if (err) throw err;
-    res.json(JSON.parse(data));
-  });
+  const notes = JSON.parse(fs.readFileSync(path.join(__dirname, '/db/db.json'), 'utf8'));
+  res.json(notes);
 });
 
+// Receive a new note, add it to the db.json file, and return the new note
 app.post('/api/notes', (req, res) => {
-  const newNote = { ...req.body, id: uuidv4() };
-  fs.readFile('./db/db.json', 'utf-8', (err, data) => {
-    if (err) throw err;
-    const notes = JSON.parse(data);
-    notes.push(newNote);
-    fs.writeFile('./db/db.json', JSON.stringify(notes), (err) => {
-      if (err) throw err;
-      res.json(newNote);
-    });
-  });
+  const newNote = req.body;
+  newNote.id = uuidv4(); // Generate a unique ID for the new note
+
+  const notes = JSON.parse(fs.readFileSync(path.join(__dirname, '/db/db.json'), 'utf8'));
+  notes.push(newNote);
+
+  fs.writeFileSync(path.join(__dirname, '/db/db.json'), JSON.stringify(notes));
+
+  res.json(newNote);
 });
 
+// Optional: DELETE route to delete a note with a given ID
 app.delete('/api/notes/:id', (req, res) => {
   const noteId = req.params.id;
-  fs.readFile('./db/db.json', 'utf-8', (err, data) => {
-    if (err) throw err;
-    const notes = JSON.parse(data);
-    const updatedNotes = notes.filter(note => note.id !== noteId);
-    fs.writeFile('./db/db.json', JSON.stringify(updatedNotes), (err) => {
-      if (err) throw err;
-      res.json({ message: 'Note deleted' });
-    });
-  });
+
+  const notes = JSON.parse(fs.readFileSync(path.join(__dirname, '/db/db.json'), 'utf8'));
+  const updatedNotes = notes.filter(note => note.id !== noteId);
+
+  fs.writeFileSync(path.join(__dirname, '/db/db.json'), JSON.stringify(updatedNotes));
+
+  res.json({ message: `Note with ID ${noteId} has been deleted` });
 });
 
-app.get('*', (req, res) =>
-  res.sendFile(path.join(__dirname, './public/index.html'))
-);
+// Route to serve the index.html file (catch-all route)
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'));
+  });
 
+  
+// Start the server and listen on the specified port
 app.listen(PORT, () => {
-  console.log(`App listening on PORT ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
